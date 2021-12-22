a5c944812cbcb28d9a815cb41da17ecddf87f4cd
Gustavo Peña <gustavo@MacBook-Air-de-Gustavo.local>
2021-12-22 16:56:55 -0600

    Init project and add first screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import CalculatorScreen from './src/screens/CalculatorScreen';
+import { styles } from './src/theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.background}>
+      <StatusBar backgroundColor='black' style='light' />
+      <CalculatorScreen />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/CalculatorScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { styles } from '../theme'
+
+export default function CalculatorScreen() {
+  return (
+    <View>
+      <Text style={styles.text}>Calculadora</Text>
+    </View>
+  )
+}
